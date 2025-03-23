15b91014e1ff0d67f1895f846c0a364acd644550
GirishIITM <23f2005217@ds.study.iitm.ac.in>
2025-03-23 07:04:27 +0530

    refactor: update file processing logic to improve handling of uploaded files

--- api/app.py
+++ api/app.py
@@ -1,30 +1,26 @@
 from flask import Flask, request, jsonify
 import os
-
+from utils.file_process import unzip_folder
 from utils.question_matching import find_similar_question
 from utils.openai_api import extract_parameters
 from utils.solution_functions import functions_dict
-from utils.file_process import unzip_folder
-
 
 tmp_dir = "tmp_uploads"
 os.makedirs(tmp_dir, exist_ok=True)
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def fun():
     return "works"
-
 
 @app.route("/api/", methods=["POST"])
 def process_file():
     question = request.form.get("question")  # Get the question from the form data
     file = request.files.get("file")  # Get the uploaded file (optional)
+    file_names = []
 
     # Handle the file processing if file is present
-
 
     matched_function, matched_description, matched_files = find_similar_question(
         question
@@ -33,22 +29,21 @@ def process_file():
     if file:
         # this will make sure that the file is saved in the tmp_uploads folder
         # and the name of the files are exactly the same as the one in the questions.json
-        unzip_folder(file, tmp_dir, matched_files) 
+        (tmp_dir,file_names) = unzip_folder(file)
         pass
 
     parameters = extract_parameters(
         str(question)
     )  # Function to call OpenAI API and extract parameters
-    
+
     solution_function = functions_dict.get(
         str(matched_function), lambda parameters: "No matching function found"
     )  # the solutions functions name is same as in questions.json
 
-    answer = solution_function(matched_function, parameters)
+    answer = solution_function(matched_function, parameters, tmp_dir, file_names)
 
     # Return the answer in JSON format
     return jsonify({"answer": answer})
-
 
 if __name__ == "__main__":
     app.run(debug=True)
